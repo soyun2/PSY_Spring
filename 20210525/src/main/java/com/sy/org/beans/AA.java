77e22e62d73a3881dd9c538f35eda5cdb768439b
soyun2 <tkdtod0619@naver.com>
2021-05-27 14:01:55 +0900

    .

---
+++ 20210525/src/main/java/com/sy/org/beans/AA.java
@@ -0,0 +1,14 @@
+package com.sy.org.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AA {
+
+	public String doA() {
+		return "doA";
+	}
+	public String doAA() {
+		return "doAA";
+	}
+}
